64048b9fd763759d06170cba7727d8085558f44d
zhengtengfei <zhengtengfei@tuandai.com>
2019-03-19 11:09:53 +0800

    添加mybatis对字段的预处理

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # personal_blog
-realy personal blog! I will finish it
+realy personal blog! I will finish it  
+1、拦截器实现登陆拦截  
+2、全局异常处理  ，JSON返回  
+3、MyBatis日期类型处理
--- blog/src/main/java/org/cherry/blog/domain/User.java
+++ blog/src/main/java/org/cherry/blog/domain/User.java
@@ -2,12 +2,18 @@ package org.cherry.blog.domain;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by zhengtengfei on 2019/3/15.
  */
 @Data
-public class User {
+public class User{
     private long id;
     private String userName;
     private String userPwd;
+    private String nickName;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    private Integer isDelete;
 }
